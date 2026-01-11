4252c8336ebfa3a12cad72310d5271fb50c572c8
danielschmal <danielschmal@outlook.de>
2026-01-11 20:24:27 +0100

    Setup: PyCharm project, venv, first tests

---
+++ setuptest/test_setup.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+import scipy
+import wquantiles
+
+print("numpy:", np.__version__)
+print("pandas:", pd.__version__)
+print("scipy:", scipy.__version__)
+print("wquantiles:", wquantiles.__version__)
+print("Setup OK ✅")
